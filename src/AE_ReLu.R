d36bcbe0736ebcd37f47ee8244fb062ef1593723
Mardelor <remy.zirnheld@telecom-sudparis.eu>
2018-09-14 17:43:23 +0200

    Adding entropy and loss saving

--- src/AE_ReLu.R
+++ src/AE_ReLu.R
@@ -7,7 +7,7 @@ library(dplyr)
 
 # Parameters
 original_dim <- 784L
-compressed_dim <- 150L
+compressed_dim <- 256L
 batch_size <- 50L
 epochs <- 50L
 
@@ -53,8 +53,8 @@ y_train <- apply(y_train, 2, round)
 y_train_df <- data.frame(y_train)
 
 # Distribution
-for (i in c(0:14)) {
-  plot(ggplot(melt(y_train_df[,(i*10 + 1):((i+1)*10)]), aes(x = value, fill = variable)) + geom_density(alpha = 0.2))
+for (i in c(0:15)) {
+  plot(ggplot(melt(y_train_df[,(i*16 + 1):((i+1)*16)]), aes(x = value, fill = variable)) + geom_density(alpha = 0.2))
   ggsave(filename = sprintf("ReLu_%dN_%d.png", compressed_dim, i))
 }
 
@@ -71,4 +71,15 @@ for (i in c(1:compressed_dim)) {
 plot(entropyAE)
 print(mean(entropyAE))
 print(sd(entropyAE))
+loss <- evaluate(fae, x_train, x_train)
+entropyAE <- c(entropyAE, sum(entropyAE), loss)
 
+names <- c(1:(compressed_dim + 2))
+for (i in c(1:compressed_dim)) {
+  names[i] <- sprintf("X%d", i)
+}
+names[compressed_dim + 1] <- "Sum"
+names[compressed_dim + 2] <- "Loss"
+entropy <- data.frame(t(entropyAE))
+names(entropy) <- names
+write.csv(entropy, file = sprintf("Loss&Entropy_ReLu_%dN.csv", compressed_dim))
--- src/AE_SoftPlus.R
+++ src/AE_SoftPlus.R
@@ -7,7 +7,7 @@ library(dplyr)
 
 # Parameters
 original_dim <- 784L
-compressed_dim <- 256L
+compressed_dim <- 150L
 batch_size <- 50L
 epochs <- 20L
 periods <- 5L
@@ -108,7 +108,6 @@ for (j in c(1:periods)) {
   hist <- fae %>% fit(
     x_train, x_train,
     epochs = 5,
-    validation_data = list(x_test, x_test),
     batch_size = batch_size
   )
   plot(hist)
@@ -126,8 +125,9 @@ y_train <- apply(y_train, 2, round)
 y_train_df <- data.frame(y_train)
 
 # Distribution
-for (i in c(0:15)) {
-  plot(ggplot(melt(y_train_df[,(i*16 + 1):((i+1)*16)]), aes(x = value, fill = variable)) + geom_density(alpha = 0.2))
+for (i in c(0:14)) {
+  plot(ggplot(melt(y_train_df[,(i*10 + 1):((i+1)*10)]), aes(x = value, fill = variable)) + geom_density(alpha = 0.2))
+  ggsave(filename = sprintf("SoftPlusmi_%dN_%d.png", compressed_dim, i))
 }
 
 # Entropy
@@ -143,6 +143,18 @@ for (i in c(1:compressed_dim)) {
 plot(entropyAE)
 print(mean(entropyAE))
 print(sd(entropyAE))
+loss <- evaluate(fae, x_train, x_train)
+entropyAE <- c(entropyAE, sum(entropyAE), loss)
+
+names <- c(1:(compressed_dim + 2))
+for (i in c(1:compressed_dim)) {
+  names[i] <- sprintf("X%d", i)
+}
+names[compressed_dim + 1] <- "Sum"
+names[compressed_dim + 2] <- "Loss"
+entropy <- data.frame(t(entropyAE))
+names(entropy) <- names
+write.csv(entropy, file = sprintf("Loss&Entropy_SoftPlusmi_%dN.csv", compressed_dim))
 
 # Apply the empirical distribution function
 y <- y_train
